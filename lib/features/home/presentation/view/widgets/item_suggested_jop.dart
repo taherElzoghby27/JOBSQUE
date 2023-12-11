8c44e695111245187b390fdd4587cc474b18e690
taherelzoghby <taheramin442@gmail.com>
2023-12-11 19:15:15 +0200

    edited item suggested jop

--- lib/features/home/presentation/view/widgets/item_suggested_jop.dart
+++ lib/features/home/presentation/view/widgets/item_suggested_jop.dart
@@ -27,138 +27,136 @@ class ItemSuggestedJop extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return BlocBuilder<SavedCubit, SavedState>(
-      builder: (context, state) {
-        bool isSaved = false;
-        if (state is OnChangedSavedState) {
-          isSaved = state.isSaved;
-        }
-        return InkWell(
-          onTap: onTap,
-          child: AspectRatio(
-            aspectRatio: 13 / 9,
+    return InkWell(
+      onTap: onTap,
+      child: AspectRatio(
+        aspectRatio: 13 / 9,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppConsts.primary900,
+              borderRadius: BorderRadius.circular(16),
+            ),
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: AppConsts.primary900,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  //logo jopName
+                  Row(
                     children: [
-                      //logo jopName
-                      Row(
+                      ///logo
+                      CustomPhoto(
+                        image: job.image!,
+                        height: size.height * .055.h,
+                      ),
+                      Spacer(),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          ///logo
-                          CustomPhoto(
-                            image: job.image!,
-                            height: size.height * .055.h,
-                          ),
-                          Spacer(),
-
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ///title
-                              SizedBox(
-                                width: size.width * .4.w,
-                                child: Text(
-                                  job.name!,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: AppConsts.style32.copyWith(
-                                    color: AppConsts.neutral100,
-                                    fontSize: 18,
-                                  ),
-                                ),
+                          ///title
+                          SizedBox(
+                            width: size.width * .4.w,
+                            child: Text(
+                              job.name!,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: AppConsts.style32.copyWith(
+                                color: AppConsts.neutral100,
+                                fontSize: 18,
                               ),
-
-                              ///company.country
-                              SizedBox(
-                                width: size.width * .4.w,
-                                child: Text(
-                                  '${job.compName}.${job.location}',
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: AppConsts.styleHint14.copyWith(
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
-                          Spacer(),
 
-                          ///bookmark
-                          IconButton(
+                          ///company.country
+                          SizedBox(
+                            width: size.width * .4.w,
+                            child: Text(
+                              '${job.compName}.${job.location}',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: AppConsts.styleHint14.copyWith(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+
+                      ///bookmark
+                      BlocBuilder<SavedCubit, SavedState>(
+                        builder: (context, state) {
+                          bool isSaved = BlocProvider.of<SavedCubit>(context)
+                              .checkSavedOrNot(job: job);
+                          return IconButton(
                             onPressed: () =>
                                 BlocProvider.of<SavedCubit>(context)
-                                    .onChangeSaved(),
+                                    .onChangeSaved(job: job),
                             icon: Icon(
                               FontAwesomeIcons.bookmark,
                               color: isSaved
                                   ? AppConsts.primary500
                                   : AppConsts.neutral100,
                             ),
-                          )
-                        ],
-                      ),
-                      //full time -remote -design
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Expanded(
-                            child: TypeJopComponent(
-                              label: job.jobTimeType!,
-                              color: AppConsts.neutral100,
-                            ),
-                          ),
-                          Expanded(
-                            child: TypeJopComponent(
-                              label: StringsEn.remote,
-                              color: AppConsts.neutral100,
-                            ),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                      //salary - apply jop
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                '\$${job.salary}',
-                                style: AppConsts.style20,
-                              ),
-                              Text(
-                                '/${StringsEn.month}',
-                                style: AppConsts.style14,
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: size.height * .045.h,
-                            width: size.width * .28.w,
-                            child: CustomButton(
-                              text: StringsEn.applyNow,
-                              onTap: onTapApplyNow,
-                            ),
-                          )
-                        ],
+                          );
+                        },
                       ),
                     ],
                   ),
-                ),
+                  //full time -remote -design
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        child: TypeJopComponent(
+                          label: job.jobTimeType!,
+                          color: AppConsts.neutral100,
+                        ),
+                      ),
+                      Expanded(
+                        child: TypeJopComponent(
+                          label: StringsEn.remote,
+                          color: AppConsts.neutral100,
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                  //salary - apply jop
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            '\$${job.salary}',
+                            style: AppConsts.style20,
+                          ),
+                          Text(
+                            '/${StringsEn.month}',
+                            style: AppConsts.style14,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: size.height * .045.h,
+                        width: size.width * .28.w,
+                        child: CustomButton(
+                          text: StringsEn.applyNow,
+                          onTap: onTapApplyNow,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
