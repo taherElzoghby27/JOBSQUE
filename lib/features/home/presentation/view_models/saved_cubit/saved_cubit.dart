48072eb009f8ec4dadf53c48f5d5fcefd4eb9b9d
taherelzoghby <taheramin442@gmail.com>
2023-12-11 19:13:45 +0200

    edited saved cubit

--- lib/features/home/presentation/view_models/saved_cubit/saved_cubit.dart
+++ lib/features/home/presentation/view_models/saved_cubit/saved_cubit.dart
@@ -1,21 +1,53 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:jobsque/core/consts/strings.dart';
+import 'package:jobsque/core/models/job_model.dart';
 import 'package:meta/meta.dart';
 
 part 'saved_state.dart';
 
 class SavedCubit extends Cubit<SavedState> {
   SavedCubit() : super(SavedInitial());
-  bool isSaved = false;
+  Box<Job> savedJobBox = Hive.box<Job>(StringsEn.savedJobsBox);
+  List<Job> savedJobsList = [];
 
   //changed saved
-  onChangeSaved() {
+  void onChangeSaved({required Job job}) {
+    final isSaved = savedJobBox.containsKey(job.id as int);
     if (isSaved) {
-      //un save job to hive
-      isSaved = false;
+      deleteJobToSavedBoxHive(job: job);
     } else {
-      // save job to hive
-      isSaved = true;
+      addJobToSavedBoxHive(job: job);
     }
-    emit(OnChangedSavedState(isSaved: isSaved));
+
+    emit(OnChangedSavedState(isSaved: !isSaved));
+  }
+
+  //check saved or not
+  bool checkSavedOrNot({required Job job}) =>
+      savedJobBox.containsKey(job.id as int);
+
+//get all saved jobs
+  getSavedJobs() {
+    try {
+      emit(SavedJobsLoading());
+      List<Job> savedJobs = List<Job>.from(
+        (savedJobBox.values).map((job) => job),
+      ).toList();
+      savedJobsList = savedJobs;
+      emit(SavedJobsLoaded(savedJobs: savedJobsList));
+    } catch (e) {
+      emit(SavedJobsFailure(message: e.toString()));
+    }
+  }
+
+  //add job to saved box hive
+  addJobToSavedBoxHive({required Job job}) {
+    savedJobBox.put(job.id as int, job);
+  }
+
+  //delete job from saved box hive
+  deleteJobToSavedBoxHive({required Job job}) {
+    savedJobBox.delete(job.id as int);
   }
 }
--- lib/features/home/presentation/view_models/saved_cubit/saved_state.dart
+++ lib/features/home/presentation/view_models/saved_cubit/saved_state.dart
@@ -4,9 +4,24 @@ part of 'saved_cubit.dart';
 abstract class SavedState {}
 
 class SavedInitial extends SavedState {}
+
 //saved or not
 class OnChangedSavedState extends SavedState {
   final bool isSaved;
 
   OnChangedSavedState({required this.isSaved});
+}
+
+class SavedJobsLoaded extends SavedState {
+  final List<Job> savedJobs;
+
+  SavedJobsLoaded({required this.savedJobs});
+}
+
+class SavedJobsLoading extends SavedState {}
+
+class SavedJobsFailure extends SavedState {
+  final String message;
+
+  SavedJobsFailure({required this.message});
 }
