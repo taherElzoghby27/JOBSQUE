107859c434c0e26f8f45a52d087507f820581f7e
taherelzoghby <taheramin442@gmail.com>
2024-01-10 20:03:50 +0200

    added simple animation to saved icon

--- lib/core/widgets/bookmark_widget.dart
+++ lib/core/widgets/bookmark_widget.dart
@@ -6,31 +6,75 @@ import 'package:jobsque/features/home/presentation/view_models/saved_cubit/saved
 
 import '../consts/style.dart';
 
-class BookmarkWidget extends StatelessWidget {
+class BookmarkWidget extends StatefulWidget {
   const BookmarkWidget({
     super.key,
     required this.job,
     this.color = AppConsts.neutral900,
   });
-
   final Job job;
   final Color color;
+  @override
+  State<BookmarkWidget> createState() => _BookmarkWidgetState();
+}
+
+class _BookmarkWidgetState extends State<BookmarkWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> sizeAnimation;
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 250,
+      ),
+    );
+    sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem(
+          tween: Tween(begin: 25, end: 30),
+          weight: 50,
+        ),
+        TweenSequenceItem(
+          tween: Tween(begin: 30, end: 25),
+          weight: 50,
+        ),
+      ],
+    ).animate(controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () =>
-          BlocProvider.of<SavedCubit>(context).onChangeSaved(job: job),
-      icon: BlocBuilder<SavedCubit, SavedState>(
-        builder: (context, state) {
-          bool isSaved =
-              BlocProvider.of<SavedCubit>(context).checkSavedOrNot(job: job);
-          return Icon(
-            FontAwesomeIcons.bookmark,
-            color: isSaved ? AppConsts.primary500 : color,
-          );
-        },
-      ),
+    return BlocBuilder<SavedCubit, SavedState>(
+      builder: (context, state) {
+        bool isSaved = BlocProvider.of<SavedCubit>(context).checkSavedOrNot(
+          job: widget.job,
+        );
+        return IconButton(
+          onPressed: () {
+            isSaved ? controller.reverse() : controller.forward();
+            BlocProvider.of<SavedCubit>(context).onChangeSaved(job: widget.job);
+          },
+          icon: AnimatedBuilder(
+            animation: controller,
+            builder: (context, child) {
+              return Icon(
+                FontAwesomeIcons.bookmark,
+                size: sizeAnimation.value,
+                color: isSaved ? AppConsts.primary500 : widget.color,
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
