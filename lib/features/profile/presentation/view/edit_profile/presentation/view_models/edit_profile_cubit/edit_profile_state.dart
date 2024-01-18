c0aac6b471c67381d1e5672c6f3ef4abb8464db2
taherelzoghby <taheramin442@gmail.com>
2024-01-18 21:40:41 +0200

    added cubit for edit profile

---
+++ lib/features/profile/presentation/view/edit_profile/presentation/view_models/edit_profile_cubit/edit_profile_state.dart
@@ -0,0 +1,10 @@
+part of 'edit_profile_cubit.dart';
+
+sealed class EditProfileState extends Equatable {
+  const EditProfileState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class EditProfileInitial extends EditProfileState {}
