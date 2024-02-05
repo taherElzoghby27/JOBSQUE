ac409ff001c2acbc5642aa160aeb75ff09c83dfd
taherelzoghby <taheramin442@gmail.com>
2024-02-05 14:58:50 +0200

    added failure message model

--- lib/core/errors/failure_message.dart
+++ lib/core/errors/failure_message.dart
@@ -1,11 +1,37 @@
-class FailureMessage {
-  String? message;
+import 'package:http/http.dart' as http;
 
-  FailureMessage({required this.message});
+abstract class Failure {
+  final String message;
 
-  FailureMessage.fromJson(Map<String, dynamic> json) {
-    message = (json['massege'] is String
-        ? json['massege']
-        : json['massege']['email'][0]) as String;
+  Failure({required this.message});
+}
+
+class FailureMessage extends Failure {
+  FailureMessage({required super.message});
+
+  factory FailureMessage.fromHttpError(http.Response response) {
+    switch (response.statusCode) {
+      case 400:
+      case 401:
+      case 403:
+        return FailureMessage(message: 'Unauthorised: ${response.body}');
+      case 404:
+        return FailureMessage(
+          message: 'Your request was not found, please try later',
+        );
+      case 500:
+      default:
+        return FailureMessage(
+          message:
+              'Error During Communication: Error occured while Communication with Server',
+        );
+    }
+  }
+
+  factory FailureMessage.fromJson(Map<String, dynamic> json) {
+    return FailureMessage(
+        message: (json['massege'] is String
+            ? json['massege']
+            : json['massege']['email'][0]) as String);
   }
 }
