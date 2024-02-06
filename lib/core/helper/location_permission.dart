4bd2bc11cdaa6ea3804c47d791b0d6cc26716527
taherelzoghby <taheramin442@gmail.com>
2024-02-06 19:35:33 +0200

    added function to location permission

---
+++ lib/core/helper/location_permission.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:jobsque/core/helper/custom_snack.dart';
+
+Future<bool> handleLocationPermission(BuildContext context) async {
+  bool serviceEnabled;
+  // bool? settings;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+  if (!serviceEnabled) {
+    await Geolocator.openLocationSettings();
+    return false;
+  }
+  permission = await Geolocator.checkPermission();
+
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // ignore: use_build_context_synchronously
+      showSnack(
+        context,
+        message: 'Location permissions are denied',
+      );
+      return false;
+    }
+  }
+  if (permission == LocationPermission.deniedForever) {
+    // ignore: use_build_context_synchronously
+    showSnack(
+      context,
+      message: 'Location permissions are permanently denied, we cannot request permissions.',
+    );
+    return false;
+  }
+  return true;
+}
+//
+// void _openLocationPermissionSettings() async {
+//   final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+//   final isPermissionGranted = await Permission.location.status.isGranted;
+//
+//   if (!isPermissionGranted) {
+//     // Location permission is not granted. Display a message or handle the error.
+//     // For example:
+//     // ScaffoldMessenger.of(context).showSnackBar(
+//     //   SnackBar(content: Text('Location permission is required.')),
+//     // );
+//     return;
+//   }
+//
+//   if (packageInfo.packageName == 'com.android.settings') {
+//     // Open the location permission settings on Android.
+//     await PermissionHandler().openAppSettings();
+//   } else {
+//     // Open the app settings on iOS.
+//     await PermissionHandler().openSettings();
+//   }
+// }}
