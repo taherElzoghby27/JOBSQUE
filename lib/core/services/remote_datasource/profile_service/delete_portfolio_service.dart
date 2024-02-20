ab96fa45315867511340668ae3df55c25b11289f
taherelzoghby <taheramin442@gmail.com>
2024-02-20 22:06:21 +0200

    added delete portfolio service

---
+++ lib/core/services/remote_datasource/profile_service/delete_portfolio_service.dart
@@ -0,0 +1,20 @@
+import 'package:jobsque/core/consts/api.dart';
+
+import '../../../consts/api_service.dart';
+
+class DeletePortfolioService {
+  ApiService apiService;
+
+  DeletePortfolioService({required this.apiService});
+
+  //delete Portfolio method
+  Future<Map<String, dynamic>> deletePortfolio({
+    required int idPortfolio,
+  }) async {
+    Map<String, dynamic> response = await apiService.put(
+      path: "${ApiConsts.getPortfolioEndPoint}/$idPortfolio",
+    );
+
+    return response;
+  }
+}
