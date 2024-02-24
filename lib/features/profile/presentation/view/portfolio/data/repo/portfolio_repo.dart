876886ef767e6adf1bae7e7af6592a4e2faaa742
taherelzoghby <taheramin442@gmail.com>
2024-02-24 13:01:46 +0200

    edited portfolio repo

--- lib/features/profile/presentation/view/portfolio/data/repo/portfolio_repo.dart
+++ lib/features/profile/presentation/view/portfolio/data/repo/portfolio_repo.dart
@@ -1,15 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:jobsque/core/errors/failure_message.dart';
-import 'package:jobsque/core/models/user_profile_model/user_profile_portolio_model.dart';
-import 'package:jobsque/features/profile/presentation/view/portfolio/data/models/portfolio.dart';
+import 'package:jobsque/core/models/user_profile_model/portfolio.dart';
 
 abstract class PortfolioRepo {
-  //get portfolios
-  Future<Either<FailureServ, UserProfilePortfolioModel>> getPortFolio();
-
   //add portfolios
-  Future<Either<FailureServ, PortfolioCv>> addPortFolio({
-    required PortfolioCv portfolioCv,
+  Future<Either<FailureServ, PortfolioModel>> addPortFolio({
+    required PortfolioModel portfolioCv,
   });
 
   //delete portfolios
--- lib/features/profile/presentation/view/portfolio/data/repo/portfolio_repo_implementation.dart
+++ lib/features/profile/presentation/view/portfolio/data/repo/portfolio_repo_implementation.dart
@@ -1,56 +1,29 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-
-import 'package:jobsque/core/consts/api.dart';
-import 'package:jobsque/core/consts/strings.dart';
 import 'package:jobsque/core/errors/failure_message.dart';
-import 'package:jobsque/core/helper/cache_helper.dart';
-import 'package:jobsque/core/models/user_profile_model/user_profile_portolio_model.dart';
+import 'package:jobsque/core/models/user_profile_model/portfolio.dart';
 import 'package:jobsque/core/services/remote_datasource/profile_service/delete_portfolio_service.dart';
-import 'package:jobsque/features/profile/presentation/view/portfolio/data/models/portfolio.dart';
 import 'package:jobsque/features/profile/presentation/view/portfolio/data/repo/portfolio_repo.dart';
-
-import '../../../../../../../core/consts/api_service.dart';
 import '../../../../../../../core/services/remote_datasource/profile_service/add_portfolio_service.dart';
 
 class PortfolioRepoImplementation extends PortfolioRepo {
-  ApiService apiService;
   AddPortfolioService addPortfolioService;
   DeletePortfolioService deletePortfolioService;
 
   PortfolioRepoImplementation({
-    required this.apiService,
     required this.addPortfolioService,
     required this.deletePortfolioService,
   });
 
   @override
-  Future<Either<FailureServ, UserProfilePortfolioModel>> getPortFolio() async {
-    try {
-      Map<String, dynamic> result = await apiService.get(
-        path:
-            "${ApiConsts.getPortfolioEndPoint}?user_id=${CacheHelper.getData(key: StringsEn.userId)}",
-      );
-      //success
-      UserProfilePortfolioModel profileUser =
-          UserProfilePortfolioModel.fromJson(result["data"]);
-      return Right(profileUser);
-    } catch (error) {
-      if (error is DioException) {
-        return Left(ServerFailure.fromDioError(error));
-      }
-      return Left(ServerFailure(message: error.toString()));
-    }
-  }
-
-  @override
-  Future<Either<FailureServ, PortfolioCv>> addPortFolio({
-    required PortfolioCv portfolioCv,
+  Future<Either<FailureServ, PortfolioModel>> addPortFolio({
+    required PortfolioModel portfolioCv,
   }) async {
     try {
-      Map<String, dynamic> result =
-          await addPortfolioService.addPortfolio(portfolioCv: portfolioCv);
-      PortfolioCv portfolio = PortfolioCv.fromJson(result["data"]);
+      Map<String, dynamic> result = await addPortfolioService.addPortfolio(
+        portfolioCv: portfolioCv,
+      );
+      PortfolioModel portfolio = PortfolioModel.fromJson(result["data"]);
       return Right(portfolio);
     } catch (error) {
       if (error is DioException) {
